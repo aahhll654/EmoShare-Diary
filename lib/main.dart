7134cb9877d4badb5d05689541121d162b1d8ab8
seungwone <aahhll654@gmail.com>
2024-04-01 15:14:59 +0900

    gorouter 및 riverpod 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
-import 'package:emoshare_diary/common/view/root_tab.dart';
+import 'package:emoshare_diary/common/provider/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  initializeDateFormatting().then((_) => runApp(const _App()));
+  initializeDateFormatting().then(
+    (_) => runApp(
+      const ProviderScope(
+        child: _App(),
+      ),
+    ),
+  );
 }
 
-class _App extends StatelessWidget {
+class _App extends ConsumerWidget {
   const _App();
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: RootTab(),
+      routerConfig: router,
     );
   }
 }
---
+++ lib/user/provider/auth_provider.dart
@@ -0,0 +1,40 @@
+import 'package:emoshare_diary/common/view/root_tab.dart';
+import 'package:emoshare_diary/common/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final authProvider = ChangeNotifierProvider<AuthProvider>(
+  (ref) {
+    return AuthProvider(ref: ref);
+  },
+);
+
+class AuthProvider extends ChangeNotifier {
+  final Ref ref;
+
+  AuthProvider({
+    required this.ref,
+  });
+
+  List<GoRoute> get routes => [
+        GoRoute(
+          path: '/',
+          name: RootTab.routeName,
+          builder: (_, __) => const RootTab(),
+        ),
+        GoRoute(
+          path: '/splash',
+          name: SplashScreen.routeName,
+          builder: (_, __) => const SplashScreen(),
+        ),
+      ];
+
+  String? redirectLogic(BuildContext _, GoRouterState state) {
+    if (state.fullPath == '/splash') {
+      return '/';
+    }
+
+    return null;
+  }
+}
