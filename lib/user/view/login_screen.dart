7d2eb1c5db833725794abcda270bf14b1aaa76d2
seungwone <aahhll654@gmail.com>
2024-10-08 16:10:14 +0900

    Feat: Firebase Authentication과 구글 소셜 로그인 연동 구현

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,11 +1,21 @@
 import 'package:emoshare_diary/common/const/colors.dart';
 import 'package:emoshare_diary/common/layout/default_layout.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static String get routeName => 'login';
 
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isLoggingIn = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,15 +42,50 @@ class LoginScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  // TODO: Google 로그인 기능 구현
-                },
-                child: Image.asset(
-                  'assets/icons/google_login.png',
-                  height: 50,
+              if (isLoggingIn)
+                const CircularProgressIndicator()
+              else
+                GestureDetector(
+                  onTap: () async {
+                    setState(() {
+                      isLoggingIn = true;
+                    });
+
+                    GoogleSignIn googleSignIn = GoogleSignIn();
+                    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+                    GoogleSignInAccount? account = await googleSignIn.signIn();
+
+                    if (account != null) {
+                      GoogleSignInAuthentication authentication =
+                          await account.authentication;
+
+                      OAuthCredential googleCredential =
+                          GoogleAuthProvider.credential(
+                        idToken: authentication.idToken,
+                        accessToken: authentication.accessToken,
+                      );
+
+                      UserCredential credential = await firebaseAuth
+                          .signInWithCredential(googleCredential);
+
+                      if (credential.user != null) {
+                        debugPrint(credential.user.toString());
+                        if (context.mounted) context.go('/');
+                      }
+
+                      await googleSignIn.signOut();
+                    }
+
+                    setState(() {
+                      isLoggingIn = false;
+                    });
+                  },
+                  child: Image.asset(
+                    'assets/icons/google_login.png',
+                    height: 50,
+                  ),
                 ),
-              ),
             ],
           ),
         ),
